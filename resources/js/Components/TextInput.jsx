378e2e6fc93c40f9f2657db31acb24b00e91af02
naufalpermana991 <permana7908@gmail.com>
2024-07-29 09:29:11 +0700

    feat:slicing dashboard

--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -1,23 +1,53 @@
-import { forwardRef, useEffect, useRef } from 'react';
+import { forwardRef, useEffect, useRef } from "react";
+import PropTypes from "prop-types";
 
-export default forwardRef(function TextInput({ type = 'text', className = '', isFocused = false, ...props }, ref) {
+const TextInput = forwardRef(function TextInput(props, ref) {
     const input = ref ? ref : useRef();
+    const {
+        variant,
+        className,
+        autoFocus,
+        placeholder,
+        type,
+        isFocused,
+        ...restProps
+    } = props;
 
     useEffect(() => {
-        if (isFocused) {
+        if (autoFocus || isFocused) {
             input.current.focus();
         }
-    }, []);
+    }, [autoFocus, isFocused]);
 
     return (
         <input
-            {...props}
-            type={type}
-            className={
-                'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ' +
-                className
-            }
+            {...restProps}
+            type={type || "text"}
+            className={`rounded-2xl bg-form-bg py-[13px] px-7 w-full input-primary ${className}`}
             ref={input}
+            placeholder={placeholder}
         />
     );
 });
+
+TextInput.propTypes = {
+    type: PropTypes.oneOf(["text", "password", "email", "number", "file"]),
+    name: PropTypes.string,
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    defaultValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    className: PropTypes.string,
+    variant: PropTypes.oneOf([
+        "primary",
+        "secondary",
+        "error",
+        "primary-outline",
+    ]),
+    autoComplete: PropTypes.string,
+    required: PropTypes.bool,
+    isFocused: PropTypes.bool,
+    handleChange: PropTypes.func,
+    placeholder: PropTypes.string,
+    isError: PropTypes.bool,
+};
+
+export default TextInput;
